7c12f0e7bfe911c02491923aea9c99ba87118af3
Vishnu Vijayan <vvijayan@purdue.edu>
2017-05-21 13:27:04 +0530

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  moduleId: module.id,
+  templateUrl: './app.component.html',
+})
+export class AppComponent  { name = 'Angular'; }
---
+++ src/app/components/jumbotron/jumbotron.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+    moduleId:module.id,
+    selector: 'jumbotron',
+    templateUrl: 'jumbotron.component.html',
+})
+export class JumbotronComponent  {
+    private jbtHeading:string;
+    private jbtText:string;
+    private jbtBtnText:string;
+    private jbtBtnUrl:string;
+
+    constructor() {
+        this.jbtHeading = 'Hello World';
+        this.jbtText = 'Example text'
+        this.jbtBtnText = 'Read More';
+        this.jbtBtnUrl = '/about';
+    }
+}
